08df3daa3baf7b68e903ead4eddccce6861aa9c0
十二桥 <2512410481@qq.com>
2018-12-14 09:32:47 +0800

    合并

---
+++ NetCoreApp/Controllers/HttpApi/IOnlineStudyAPI.cs
@@ -0,0 +1,24 @@
+﻿using cdutcm.Common.Core;
+using NetCoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiClient;
+using WebApiClient.Attributes;
+
+namespace NetCoreApp.Controllers
+{
+    /// <summary>
+    /// 用户操作接口
+    /// </summary>
+    public interface IOnlineStudyAPI : IHttpApi
+    {
+        // GET {url}?account={account}&password={password}&something={something}
+        [HttpPost("Data/GetStatistics")]
+        [Timeout(10 * 1000)] // 10s超时
+        [JsonReturn]
+        ITask<AjaxResult<StatisticsDto>> GetByIdAsync([JsonContent]SearchDto query);
+
+    }
+}
