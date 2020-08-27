8ee11854e1445f7a0884b4f7cf87f24951a55079
Eom <2512410481@qq.com>
2020-08-27 16:36:36 +0800

    深拷贝

--- Common/Helpers/ExtendHelper.cs
+++ Common/Helpers/ExtendHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace Eom.Common.Helpers
@@ -14,6 +18,38 @@ namespace Eom.Common.Helpers
         public static bool IsEmpty(this string source)
         {
             return string.IsNullOrEmpty(source) || string.IsNullOrWhiteSpace(source);
+        }
+        /// <summary>
+        /// 深拷贝
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="List">The list.</param>
+        /// <returns>List{``0}.</returns>
+        public static List<T> DeepCopy<T>(List<object> List)
+        {
+            using (Stream objectStream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectStream, List);
+                objectStream.Seek(0, SeekOrigin.Begin);
+                return formatter.Deserialize(objectStream) as List<T>;
+            }
+        }
+        /// <summary>
+        /// 深拷贝
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The list.</param>
+        /// <returns>T</returns>
+        public static T DeepCopy<T>(object obj)
+        {
+            using (Stream objectStream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectStream, obj);
+                objectStream.Seek(0, SeekOrigin.Begin);
+                return (formatter.Deserialize(objectStream) as List<T>).FirstOrDefault();
+            }
         }
     }
 }
